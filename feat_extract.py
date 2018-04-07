64ed964f8203213eb929b7c2530c116b9351f299
yuchen-zhang <yuchen_zhang@apple.com>
2018-04-07 15:00:05 -0400

    Added feat_extract.py

--- feat_extract.py
+++ feat_extract.py
@@ -3,19 +3,29 @@ import numpy as np
 
 def extractFeature(wav_file):
   y, sr = librosa.load(wav_file, sr=44100)
-  audio_len_sec = librosa.core.get_duration(y, sr)
+
+  # Split and remove silence
+  split_begin_end = librosa.effects.split(y)
+  y_ = np.array([])
+  for start_time, end_time in split_begin_end:
+    y_ = np.concatenate([y_, y[start_time:end_time]], axis=0)
+
+  audio_len_sec = librosa.core.get_duration(y_, sr)
+  print("Sample length: {0}".format(y_.shape[0]))
   print("Audio length: {0} seconds".format(audio_len_sec))
-  # S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
-  mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-  # mfcc_d = librosa.feature.delta(mfcc)
-  # mfcc_dd = librosa.feature.delta(mfcc_d)
+  mfcc = librosa.feature.mfcc(y=y_, sr=sr, n_mfcc=40)
   nsize = mfcc.shape[1]
 
   print("Feature length: {0}".format(nsize))
   print("Extraction done!")
+  return mfcc
 
 def main():
+  # 44100 frames per second
+  # 1 feature per 512 frames
   extractFeature('./data/fart_clean1_mono.wav')
+  extractFeature('./data/negative_sample_party_mono.wav')
+  extractFeature('./data/negative_sample_voice_mono.wav')
 
 if __name__ == "__main__":
   main()
